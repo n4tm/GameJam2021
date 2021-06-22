30c297f1e281b1e4ea5f7649d864d49b2c89f26a
n4tm <nldm.eng20@uea.edu.br>
2021-06-22 18:08:33 -0400

    add tower attack system

--- Assets/Scripts/Tower/TowerAttack.cs
+++ Assets/Scripts/Tower/TowerAttack.cs
@@ -4,6 +4,18 @@ namespace Tower
 {
     public class TowerAttack : MonoBehaviour
     {
-        
+        [SerializeField] private Projectile projectile;
+        [SerializeField] private GameObject target;
+        private Projectile newProjectile;
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.CompareTag("Enemy") && newProjectile == null)
+            {
+                newProjectile = Instantiate(projectile, transform.position, Quaternion.identity, transform);
+                target = other.gameObject;
+                newProjectile.SetTargetTransform(target.transform);
+            }
+        }
     }
 }
